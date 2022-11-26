1c98e52f9eccfc5db345962b39cb2be9a72b0c0c
sikdong <zmstnv1119@naver.com>
2022-11-26 13:42:29 +0900

    윤동석 게시물 리스트 기능 추가

--- src/main/java/com/trips/service/ydsBoardService/ydsBoardService.java
+++ src/main/java/com/trips/service/ydsBoardService/ydsBoardService.java
@@ -1,5 +1,20 @@
 package com.trips.service.ydsBoardService;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.trips.domain.ydsBoard.tripsBoardDto;
+import com.trips.mapper.ydsBoardMapper.ydsBoardMapper;
+
 public class ydsBoardService {
+	
+	@Autowired
+	private ydsBoardMapper mapper;
+
+	public List<tripsBoardDto> getBoardlist() {
+		// TODO Auto-generated method stub
+		return mapper.getBoardList();
+	}
 
 }
