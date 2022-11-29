8a0d4ec17770b94ce1f8bd643404659e4488e425
numet <numet@61.84.131.122>
2022-11-30 08:55:41 +0900

    SecondCommit

---
+++ src/main/java/com/trips/service/jjhMember/jjhMemberService.java
@@ -0,0 +1,39 @@
+package com.trips.service.jjhMember;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.trips.domain.jjhMember.jjhMemberDto;
+import com.trips.mapper.jjhMember.jjhMemberMapper;
+
+@Service
+@Transactional
+public class jjhMemberService {
+
+	@Autowired
+	private jjhMemberMapper memberMapper;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	public int insert(jjhMemberDto member) {
+		
+		String pw = member.getPassword();
+		
+		member.setPassword(passwordEncoder.encode(pw));
+		
+		return memberMapper.insert(member);
+	}
+	
+	public jjhMemberDto getById(String id) {
+		// TODO Auto-generated method stub
+		return memberMapper.selectById(id);
+	}
+	
+	public jjhMemberDto getByEmail(String email) {
+		// TODO Auto-generated method stub
+		return memberMapper.selectByEmail(email);
+	}
+}
