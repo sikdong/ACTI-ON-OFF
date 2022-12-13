31300ce3e669193f9b157d5dfa9b1fe28a33ffbb
seosum <ssm347574248@gmail.com>
2022-12-13 17:29:01 +0900

    커밋완료

--- src/main/java/com/trips/domain/payment/testDto.java
+++ src/main/java/com/trips/domain/payment/testDto.java
@@ -1,11 +1,16 @@
 package com.trips.domain.payment;
 
+import java.sql.Date;
+
 import lombok.Data;
 
 @Data
 public class testDto {
 	private int memberId;
+	private int id;
 	private int boardNumber;
 	private int cartId;
+	private Date addDate; 
 	private int price; 
+	private String renamedFilename;
 }
